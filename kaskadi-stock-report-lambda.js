1d619d1fae4bfdc947072b8a7fb7563cd7ff10ce
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-04 14:31:56 +0100

    Basic setup for testing

--- kaskadi-stock-report-lambda.js
+++ kaskadi-stock-report-lambda.js
@@ -1,11 +1,11 @@
+const AWS = require('aws-sdk')
+const sns = new AWS.SNS({apiVersion: '2010-03-31'})
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      message: 'Your lambda is running!'
-    })
+  const params = {
+    Message: 'This is a test',
+    Subject: 'Test publish from Lambda',
+    TopicArn: process.env.TOPIC_ARN
   }
+  await sns.publish(params).promise()
 }
