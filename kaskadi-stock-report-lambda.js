ea7ca2de81ff1e75712d3a0d87f57a3f6f0fb943
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-04 17:00:47 +0100

    Added request to ES for product data

--- kaskadi-stock-report-lambda.js
+++ kaskadi-stock-report-lambda.js
@@ -2,28 +2,46 @@ const AWS = require('aws-sdk')
 const sns = new AWS.SNS({apiVersion: '2010-03-31'})
 const WemaloClient = require('wemalo-api-wrapper')
 const client = new WemaloClient({token: process.env.WEMALO_TOKEN})
+const es = require('aws-es-client')({
+  id: process.env.ES_ID,
+  token: process.env.ES_SECRET,
+  url: process.env.ES_ENDPOINT
+})
 
 module.exports.handler = async (event) => {
   const prods = await client.getAllProducts()
   let productsData = []
   for (const product of prods.products) {
-    const productInfo = await Promise.all([client.getStockReduced(product.externalId, false)]) // TODO: query ES in parallel to get buying price
-    console.log(productInfo)
+    const productInfo = await Promise.all([
+      client.getStockReduced(product.externalId, true),
+      es.get({
+        id: product.externalId,
+        index: 'products'
+      })
+    ])
     productsData.push({
       id: product.externalId,
       sku: product.sku,
       ean: product.ean,
+      name: productInfo[1].name,
+      buyingPrice: productInfo[1].buyingPrice,
       quantity: productInfo[0].quantity,
-      notReservedQuantity: productInfo[0].notReservedQuantity
-    }) // TODO: add field for buying price pulling info from productInfo[1]
+      quantityReserved: productInfo[0].quantityReserved
+    })
   }
   let msg = ''
   productsData.forEach(productData => {
-    msg += `ID: ${productData.id}
+    msg += `${productData.name}
+
+ID: ${productData.id}
 SKU: ${productData.sku}
 EAN: ${productData.ean}
-Quantity: ${productData.quantity || 'N/A'}
-Not reserved quantity: ${productData.notReservedQuantity || 'N/A'}
+Buying price: ${productData.buyingPrice || 'N/A'}
+Quantity: ${productData.quantity || '0'}
+Reserved quantity: ${productData.quantityReserved || '0'}
+
+-----------------
+
 `
   })
   const params = {
