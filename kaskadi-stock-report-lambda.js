3d6c762b5acc9975b1a696e83c3f6d206cde55b4
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-04 16:06:45 +0100

    Testing with YSWS API call

--- kaskadi-stock-report-lambda.js
+++ kaskadi-stock-report-lambda.js
@@ -1,9 +1,12 @@
 const AWS = require('aws-sdk')
 const sns = new AWS.SNS({apiVersion: '2010-03-31'})
+const WemaloClient = require('wemalo-api-wrapper')
+const client = new WemaloClient({token: process.env.WEMALO_TOKEN})
 
 module.exports.handler = async (event) => {
+  const prods = await client.getAllProducts()
   const params = {
-    Message: 'This is a test',
+    Message: JSON.stringify(prods),
     Subject: 'Test publish from Lambda',
     TopicArn: process.env.TOPIC_ARN
   }
