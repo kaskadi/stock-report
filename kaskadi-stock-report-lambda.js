767184045001d90dc95cb9de530fd11c4e7fb61b
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-04 16:35:58 +0100

    Testing with more complex data

--- kaskadi-stock-report-lambda.js
+++ kaskadi-stock-report-lambda.js
@@ -5,10 +5,31 @@ const client = new WemaloClient({token: process.env.WEMALO_TOKEN})
 
 module.exports.handler = async (event) => {
   const prods = await client.getAllProducts()
-  console.log(prods)
+  let productsData = []
+  for (const product of prods.products) {
+    const productInfo = await Promise.all([client.getStockReduced(product.externalId, true)]) // TODO: query ES in parallel to get buying price
+    productsData.push({
+      id: product.externalId,
+      sku: product.sku,
+      ean: product.ean,
+      quantity: productInfo[0].quantity,
+      notReservedQuantity: productInfo[0].notReservedQuantity
+    }) // TODO: add field for buying price pulling info from productInfo[1]
+  }
+  let msg = ''
+  productsData.forEach(productData => {
+    msg += `<div>
+  <div>ID: ${productData.id}</div>
+  <div>SKU: ${productData.sku}</div>
+  <div>EAN: ${productData.ean}</div>
+  <div>Quantity: ${productData.quantity}</div>
+  <div>Not reserved quantity: ${productData.notReservedQuantity}</div>
+</div>
+`
+  })
   const params = {
-    Message: JSON.stringify(prods),
-    Subject: 'Test publish from Lambda',
+    Message: msg,
+    Subject: 'Weekly stock update',
     TopicArn: process.env.TOPIC_ARN
   }
   await sns.publish(params).promise()
