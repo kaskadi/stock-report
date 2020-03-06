4a3567a080c90945d54154324d87ef21e4c514a3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-06 17:36:38 +0100

    Fixed syntax

--- helpers/build-msg.js
+++ helpers/build-msg.js
@@ -1,22 +1,22 @@
 module.exports = (dbData) => {
-  return dbData.map(data => `${getProductData(data)}
+  return dbData.map(data => `${data._source.name}
+
+${getProductData(data)}
 
 ${getStocks(data._source.stocks)}
 
 -----------------`).join('\n\n')
 }
 
-function getProductData(data) => {
-  return `${data._source.name}
-
-Product data:
+function getProductData(data) {
+  return `Product data:
   ID: ${data._id}
   SKU: ${data._source.sku}
   EAN: ${data._source.ean}
   Buying price: ${data._source.buyingPrice || 'no information'}`
 }
 
-function getStocks(stocks) => {
+function getStocks(stocks) {
   return Object.entries(stocks).map(entry => `${entry[0]} stocks:
   Quantity: ${entry[1].amount}`).join('\n\n')
 }
