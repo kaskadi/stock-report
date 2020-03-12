f94f5db8a7729b3950561ee62e7ea73c1bd837e9
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-12 16:15:22 +0100

    Added promisified sendmail utility

--- kaskadi-stock-report-lambda.js
+++ kaskadi-stock-report-lambda.js
@@ -1,23 +1,13 @@
 const searchAllProducts = require('search-all-products.js')
 const buildMsg = require('build-msg.js')
-
-const sendmail = require('sendmail')()
+const sendEmail = require('send-email.js')
 
 module.exports.handler = async (event) => {
   const dbData = await searchAllProducts()
-  sendmail({
+  await sendEmail({
     from: 'no-reply@klimapartner.net',
     to: 'a.lemaire@klimapartner.de',
     subject: 'Weekly stock report',
     html: buildMsg(dbData)
-  }, function(err, reply) {
-    console.log(err && err.stack)
-    console.dir(reply)
   })
-  // const params = {
-  //   Message: buildMsg(dbData),
-  //   Subject: 'Weekly stock report',
-  //   TopicArn: process.env.TOPIC_ARN
-  // }
-  // await sns.publish(params).promise()
 }
---
+++ layer/nodejs/utils/send-email.js
@@ -0,0 +1,13 @@
+const sendmail = require('sendmail')()
+
+module.exports = (opts) => {
+  return new Promise((resolve, reject) => {
+    sendmail(opts, function(err, reply) {
+      console.log(reply)
+      if (err !== null) {
+        console.log(err && err.stack)
+      }
+      resolve()
+    })
+  })
+}
