1f5c8f083acca56aa9f1d755d6b5e82fd81d7354
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-03-13 16:38:33 +0100

    Updated email colors + added week number in subject

--- helpers/build-msg.js
+++ helpers/build-msg.js
@@ -6,7 +6,7 @@ module.exports = (dbData) => {
 }
 
 function getProductData(data) {
-  return `<div style="font-weight: bold; color: chartreuse;">Product data:</div>
+  return `<div style="font-weight: bold; color: darkorange;">Product data:</div>
 <div>ID: ${data._id}</div>
 <div>SKU: ${data._source.sku}</div>
 <div>EAN: ${data._source.ean}</div>
@@ -18,6 +18,6 @@ function getStocks(stocks) {
     ysws: 'YouSellWeSend',
     amz_de: 'Amazon.de'
   }
-  return Object.entries(stocks).map(entry => `<div style="font-weight: bold; color: chartreuse;">${warehouseNamesMap[entry[0]]} stocks:</div>
+  return Object.entries(stocks).map(entry => `<div style="font-weight: bold; color: darkorange;">${warehouseNamesMap[entry[0]]} stocks:</div>
 <div>Quantity: ${entry[1].amount}</div>`).join('')
 }
--- kaskadi-stock-report-lambda.js
+++ kaskadi-stock-report-lambda.js
@@ -1,15 +1,17 @@
 const searchAllProducts = require('./helpers/search-all-products.js')
 const buildMsg = require('./helpers/build-msg.js')
 const listRecipients = require('./helpers/list-recipients.js')
+const getWeekNumber = require('./helpers/get-week-number.js')
 const sendEmail = require('./helpers/send-email.js')
 
 module.exports.handler = async (event) => {
   const dbData = await searchAllProducts()
   const recipients = await listRecipients()
+  const weekN = getWeekNumber(new Date())
   await sendEmail({
     from: 'stock-reporter@klimapartner.net',
     to: recipients,
-    subject: 'Weekly stock report',
+    subject: `Weekly stock report - week ${weekN}`,
     html: buildMsg(dbData)
   })
 }
