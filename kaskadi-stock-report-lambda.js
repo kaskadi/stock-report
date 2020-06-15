1b6edfa3b5d7bcfaa78395c115a657872a8604fb
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-15 13:48:36 +0200

    Optimization: parallelized requests to ES

--- kaskadi-stock-report-lambda.js
+++ kaskadi-stock-report-lambda.js
@@ -1,17 +1,14 @@
-const searchAllProducts = require('./helpers/search-all-products.js')
-const buildMsg = require('./helpers/build-msg.js')
-const listRecipients = require('./helpers/list-recipients.js')
-const getWeekNumber = require('./helpers/get-week-number.js')
-const sendEmail = require('./helpers/send-email.js')
-
 module.exports.handler = async (event) => {
-  const dbData = await searchAllProducts()
-  const recipients = await listRecipients()
-  const weekN = getWeekNumber(new Date())
-  await sendEmail({
+  const listRecipients = require('./helpers/list-recipients.js')
+  const searchAllProducts = require('./helpers/search-all-products.js')
+  const getWeekNumber = require('./helpers/get-week-number.js')
+  const buildMsg = require('./helpers/build-msg.js')
+  const sendEmail = require('./helpers/send-email.js')
+  await Promise.all([listRecipients(), searchAllProducts()])
+  .then(data => sendEmail({
     from: 'stock-reporter@klimapartner.net',
-    to: recipients,
-    subject: `Weekly stock report - week ${weekN}`,
-    html: buildMsg(dbData)
-  })
+    to: data[0],
+    subject: `Weekly stock report - week ${getWeekNumber(new Date())}`,
+    html: buildMsg(data[1])
+  }))
 }
