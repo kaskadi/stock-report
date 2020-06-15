f5a54ab497f1dcf4eef24add90a696739e77b876
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-15 10:58:52 +0200

    Hotfix: updated code for new stocks data structure

--- helpers/build-msg.js
+++ helpers/build-msg.js
@@ -1,7 +1,7 @@
 module.exports = (dbData) => {
   return dbData.map(data => `<h1 style="color: dodgerblue;">${data._source.name}</h1>
 <div style="padding: 10px 0 5px 0;">${getProductData(data)}</div>
-<div style="padding: 5px 0 10px 0;">${getStocks(data._source.stocks)}</div>
+<div style="padding: 5px 0 10px 0;">${getStocksData(data._source.stocks)}</div>
 <hr>`).join('')
 }
 
@@ -13,11 +13,30 @@ function getProductData(data) {
 <div>Buying price: ${`${data._source.purchasePrice}€` || 'no information'}</div>`
 }
 
-function getStocks(stocks) {
+function getStocksData(stocks) {
   const warehouseNamesMap = {
     ysws: 'YouSellWeSend',
-    amz_de: 'Amazon.de'
+    amz_de: 'Amazon DE',
+    amz_gb: 'Amazon GB',
+    amz_fr: 'Amazon FR',
+    amz_es: 'Amazon ES',
+    amz_it: 'Amazon IT',
+    amz_ca: 'Amazon CA',
+    amz_mx: 'Amazon MX',
+    amz_us: 'Amazon US',
+    amz_au: 'Amazon AU',
+    amz_jp: 'Amazon JP',
+    amz_ae: 'Amazon AE',
+    amz_in: 'Amazon IN',
+    amz_tr: 'Amazon TR',
+    amz_cn: 'Amazon CN'
   }
   return Object.entries(stocks).map(entry => `<div style="font-weight: bold; color: darkorange;">${warehouseNamesMap[entry[0]]} stocks:</div>
-<div>Quantity: ${entry[1].amount}</div>`).join('')
+<ul>
+  ${entry[1].stockData.map(data => `<li>
+    <div>${entry[0].toUpperCase()}: ${data.id}</div>
+    <div>Quantity: ${data.quantity}</div>
+    <div>Condition: ${data.condition || 'no data'}</div>
+  </li>`).join('')}
+</ul>`).join('')
 }
