10896d6268240021d3b7564ffd85ba2f05639db5
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-06-29 18:32:53 +0200

    Hotfix: updated formatting

--- helpers/build-msg.js
+++ helpers/build-msg.js
@@ -10,7 +10,7 @@ function getProductData(data) {
 <div>ID: ${data._id}</div>
 <div>SKU: ${data._source.sku}</div>
 <div>EAN: ${data._source.ean}</div>
-<div>Buying price: ${`${data._source.purchasePrice}€` || 'no information'}</div>`
+<div>Buying price: ${data._source.purchasePrice.length > 0 ? `${data._source.purchasePrice}€` : 'no information'}</div>`
 }
 
 function getStocksData(stocks) {
@@ -34,16 +34,18 @@ function getStocksData(stocks) {
   <h2 style="color: darkorange; margin: 0;">${warehouseNamesMap[entry[0]]} stocks:</h2>
   <table style="border: 1px solid black; border-collapse: collapse;">
     <tr>
+      <th style="border: 1px solid black; border-collapse: collapse; padding: 0 10px;">ASIN</th>
       <th style="border: 1px solid black; border-collapse: collapse; padding: 0 10px;">SellerSKU</th>
       <th style="border: 1px solid black; border-collapse: collapse; padding: 0 10px;">Quantity</th>
       <th style="border: 1px solid black; border-collapse: collapse; padding: 0 10px;">Condition</th>
       <th style="border: 1px solid black; border-collapse: collapse; padding: 0 10px;">Fulfillment channel</th>
     </tr>
-    ${entry[1].flatMap(asinData => asinData.stockData.flatMap(data => `<tr>
-      <td style="border: 1px solid black; border-collapse: collapse; padding: 0 10px; text-align: center;">${data.sellerSku}</td>
-      <td style="border: 1px solid black; border-collapse: collapse; padding: 0 10px; text-align: center;">${data.quantity}</td>
-      <td style="border: 1px solid black; border-collapse: collapse; padding: 0 10px; text-align: center;">${data.condition || 'no data'}</td>
-      <td style="border: 1px solid black; border-collapse: collapse; padding: 0 10px; text-align: center;">${data.fulfilmentChannel}</td>
+    ${entry[1].flatMap(asinData => asinData.stockData.map(data => `<tr>
+    <td style="border: 1px solid black; border-collapse: collapse; padding: 0 10px; text-align: center;">${asinData.asin}</td>
+    <td style="border: 1px solid black; border-collapse: collapse; padding: 0 10px; text-align: center;">${data.sellerSku}</td>
+    <td style="border: 1px solid black; border-collapse: collapse; padding: 0 10px; text-align: center;">${data.quantity}</td>
+    <td style="border: 1px solid black; border-collapse: collapse; padding: 0 10px; text-align: center;">${data.condition || 'no data'}</td>
+    <td style="border: 1px solid black; border-collapse: collapse; padding: 0 10px; text-align: center;">${data.fulfilmentChannel}</td>
     </tr>`)).join('')}
   </table>
 </div>`).join('')
